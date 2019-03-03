bfd0a2f11268f29824b7c3f6d69776985404e06b
BV Satyaramwq <bvsatyaram@gmail.com>
2019-03-03 09:52:56 +0530

    Restrict login using official emails

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  ORG_EMAIL_DOMAIN = 'codeastra.com'.freeze
+
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+    if org_email_auth?
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      if @user.persisted?
+        sign_in @user, :event => :authentication #this will throw if @user is not activated
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"]
+      end
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      flash[:alert] = I18n.t('login.org_auth_failure')
     end
+
     redirect_to root_path
+  end
+
+  private
+
+  def org_email_auth?
+    request.env["omniauth.auth"].info.email.split('@').last == ORG_EMAIL_DOMAIN
   end
 end
