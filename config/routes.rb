74c9e4b3e87052948b85e625dcd940bdbfae80b5
ShiyasPH <shiyas.ph@codeastra.com>
2019-08-27 13:32:34 +0530

    Have different root path after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root 'home#welcome'
+  authenticated :user do
+    root to: 'home#dashboard', as: :authenticated_root
+  end
+  root to: 'home#welcome'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
     delete 'sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
-
-  get 'home/welcome'
-  get 'home/dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
